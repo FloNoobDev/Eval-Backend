a55c8a1fd81f6dbb9a655e57515d780903871234
FloNoobDev <77100958+FloNoobDev@users.noreply.github.com>
2023-01-24 12:48:25 +0100

    Update formValid.js

--- public/js/formValid.js
+++ public/js/formValid.js
@@ -1,11 +1,11 @@
 
-    function validate() {
-   
-        let formfields= document.forms['product'].getElementsByClassName('form-control');
+function validate() {
 
-        for(let i=0;i<formfields.length;i++){
-            if(formfields[i].value ==''){
-                formfields[i].style.background = 'red';
-            }
+    let formfields = document.forms['product'].getElementsByClassName('form-control');
+
+    for (let i = 0; i < formfields.length; i++) {
+        if (formfields[i].value == '') {
+            formfields[i].style.background = 'red';
         }
-     }
+    }
+}
